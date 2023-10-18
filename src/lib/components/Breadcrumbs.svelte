ee0645d9ed77fc499b71563b2c135d4935f98d65
Ludvig <ludvig.vigerust@evidi.com>
2023-10-18 15:33:09 +0200

    push

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -4,15 +4,15 @@
 	$: breadcrumbs = $page.route.id?.slice(1).split('/')
 </script>
 
-<ul class="flex gap-x-2 pb-6 pt-12">
-	<li><a href="/" class=" text-sm font-semibold">Start</a></li>
+<ul class="ml-2 flex gap-x-2 pb-3 pt-6 text-xs sm:ml-1 sm:pb-6 sm:pt-12 sm:text-sm">
+	<li><a href="/" class="  font-semibold">Start</a></li>
 
 	{#if breadcrumbs}
 		{#each breadcrumbs as breadcrumb}
 			{#if breadcrumb !== ''}
 				<span class="select-none">></span>
 			{/if}
-			<li><a href="/{breadcrumb}" class="text-sm font-semibold capitalize">{breadcrumb}</a></li>
+			<li><a href="/{breadcrumb}" class=" font-semibold capitalize">{breadcrumb}</a></li>
 		{/each}
 	{/if}
 </ul>
--- src/routes/saker/sak/InformationCard.svelte
+++ src/routes/saker/sak/InformationCard.svelte
@@ -6,20 +6,20 @@
 	const {
 		elements: { content, item, trigger, root },
 		helpers: { isSelected }
-	} = createAccordion()
+	} = createAccordion({ defaultValue: 'item-1' })
 </script>
 
 <div use:melt={$root} class="-my-6 text-start {$isSelected('item-1') && '-mb-2'}">
 	<div use:melt={$item('item-1')}>
 		<div
 			use:melt={$trigger('item-1')}
-			class="my-6 flex cursor-pointer items-center justify-between"
+			class="my-6 flex cursor-pointer justify-between px-1 sm:items-center sm:px-0"
 		>
 			<div>
-				<h3 class="text-lg font-semibold leading-7 text-gray-900">
-					Individsak - Tester skjema for individsak
+				<h3 class="font-semibold leading-7 text-gray-900 sm:text-lg">
+					Tester skjema for individsak
 				</h3>
-				<p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Detaljer om sak og relasjoner.</p>
+				<p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Trykk for å se detaljer.</p>
 			</div>
 			<span>
 				<svg
@@ -28,7 +28,8 @@
 					viewBox="0 0 24 24"
 					stroke-width="1.5"
 					stroke="currentColor"
-					class="h-6 w-6 {$isSelected('item-1') && 'rotate-180'} text-gray-700 transition-transform"
+					class="mt-1 h-6 w-6 sm:mt-0 {$isSelected('item-1') &&
+						'rotate-180'} text-gray-700 transition-transform"
 				>
 					<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
 				</svg>
@@ -45,33 +46,33 @@
 								Tester skjema for individsak
 							</dd>
 						</div> -->
-						<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<div class="px-1 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
 							<dt class="text-sm font-medium leading-6 text-gray-900">Tillitsvalgt</dt>
 							<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
 								Bashir Klubbstyremedlem
 							</dd>
 						</div>
-						<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<div class="px-1 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
 							<dt class="text-sm font-medium leading-6 text-gray-900">Ansettelsesforhold</dt>
 							<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
 								Kjøkkenassistent Amalie
 							</dd>
 						</div>
-						<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<div class="px-1 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
 							<dt class="text-sm font-medium leading-6 text-gray-900">Kategori</dt>
 							<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
 								Fortrinnsrett/Ansettelse
 							</dd>
 						</div>
-						<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<div class="px-1 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
 							<dt class="text-sm font-medium leading-6 text-gray-900">Beskrivelse</dt>
-							<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+							<dd class="prose mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
 								Dette er en test av skjemaet for en individsak knyttet til ansettelsesforholdet til
 								Kjøkkenassistent Amalie. Bashir, som er et klubbstyremedlem, er ansvarlig for denne
 								saken. Saken er nå avsluttet.
 							</dd>
 						</div>
-						<div class="flex items-center gap-4 px-4 py-6 sm:grid sm:grid-cols-3 sm:px-0">
+						<div class="flex items-center gap-4 px-1 py-6 sm:grid sm:grid-cols-3 sm:px-0">
 							<dt class="text-sm font-medium leading-6 text-gray-900">Status</dt>
 							<dd class="text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
 								<Badge status="Avsluttet">Avsluttet</Badge>
